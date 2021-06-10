dbcb883a7cc2c866cc00d5d7376346d55aaa7193
vikytechy <boopathiviky@gmail.com>
2021-06-10 22:45:30 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,48 @@
+# importing required modules 
+import PyPDF2
+import glob, os
+import codecs
+import docutils
+
+def convertpdf(infile,outfile):
+    pdfFileObj = open(infile, 'rb') 
+    basicfile = open(outfile, 'x')
+    outfile = codecs.open(outfile,'w',"utf-8")
+    # creating a pdf reader object 
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # printing number of pages in pdf file 
+    pages=pdfReader.numPages 
+    for page in range(0,pages):
+        # creating a page object
+        pageObj = pdfReader.getPage(page)
+        # extracting text from page 
+        pagetxt=pageObj.extractText()
+        partyst = pagetxt.find('(')
+        partyend = pagetxt.find(')')
+        party = pagetxt[partyst:partyend+1]
+        name = pagetxt[+0:partyst-1]
+        outp = name + "," + party + "\n"
+        outfile.write(outp)
+    # closing the pdf file object 
+    pdfFileObj.close()
+    outfile.close()
+    basicfile.close()
+
+def findatype(infile):
+    # this will return a tuple of root and extension
+    split_tup = os.path.splitext(infile)
+    # extract the file name and extension
+    file_name = split_tup[0]
+    file_extension = split_tup[1]
+    print("File Name: ", file_name)
+    print("File Extension: ", file_extension)
+    ofile=".txt"
+    efile=str(file_name+ofile)
+    convertpdf(infile,efile)
+# findatype(infile)
+def folderssurf(folder):
+    os.chdir(folder)
+    for file in glob.glob("*.pdf"):
+        infile=str(file)
+        findatype(infile)
+folderssurf("pdfs")
